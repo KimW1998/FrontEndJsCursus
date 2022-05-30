7b806d098e327aec0da10aa1a25baaec3e14b8c6
GROUP\1035164 <kim.wauben@conclusion.nl>
2022-05-30 14:00:18 +0200

    collom van 3 gemaakt van de Learn Cards

--- src/App.vue
+++ src/App.vue
@@ -9,22 +9,44 @@
         clita kasd gubergren, no sea takimata sanctus.
 
         <v-row>
-          <v-col :sm="12" style="max-width: 285px">
+          <v-col :sm="12"  style="max-width: 285px">
             <FeaturedCard>
               <FeaturedCardTop>
                 <div>Lorem ipsum dolor</div>
                 <FeaturedCardStat>32</FeaturedCardStat>
               </FeaturedCardTop>
               <v-card-text>
-                Behaalde punten
                 <Badge />
+                <h1>Behaalde punten</h1>
               </v-card-text>
             </FeaturedCard>
           </v-col>
-          <v-col :cols="12" md="auto grow">
-            <v-card> content </v-card>
-          </v-col>
+          <v-col>
+            <v-row>
+                <v-col :cols="12" md="auto grow">
+                  <v-card> content </v-card>
+                </v-col>
+            </v-row>
+            <v-row>
+              <v-col :cols="12" md="auto grow"  v-for="n in 3" :key="n">
+            <LearnCard>
+              <LearnCardTop>
+                <div>Lorem ipsum dolor</div>
+                <LearnCardStat>32</LearnCardStat>
+              </LearnCardTop>
+               <v-card-text>
+                <Badge />
+                <h1>Behaalde punten</h1>
+               </v-card-text>
+            </LearnCard>
+            </v-col>
+            </v-row>
+            </v-col>
+            
+            
+          
         </v-row>
+        
       </v-container>
     </v-main>
   </v-app>

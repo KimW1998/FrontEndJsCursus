0078e0db555bf46f4d6578313bcdda4f01555fa3
GROUP\1035164 <kim.wauben@conclusion.nl>
2022-06-01 13:49:55 +0200

    navigationbar created to look better but not finished

--- src/components/BannerItems/NavigationBar.vue
+++ src/components/BannerItems/NavigationBar.vue
@@ -1,33 +1,22 @@
 <template>
+
   <div class="NavigationBar">
-      <PersonalBar />
-      <v-row>
-        <v-col :sm="2">
-      <h1 class="NavigationBar--logo">CLASS</h1>
-      </v-col>
-         <v-col :sm="3">
-      <v-btn class="NavigationBar--Search">Search</v-btn>
-      </v-col>
-      <v-col :sm="1">
+    
+    <PersonalBar />
+    <v-toolbar class="NavigationBar" color="#FFFFFF" flat
+    height="80px"
+    tile>
+       <h1 class="NavigationBar--logo">CLASS</h1>
+       <v-btn class="NavigationBar--Search">Wil je iets zoeken...?</v-btn>
+      <v-spacer></v-spacer>
       <a href="test" class="NavigationBar--Link-home">Home</a>
-      </v-col>
-      <v-col :sm="1">
-      <a href="test" class="NavigationBar--Link-home">Home</a>
-      </v-col>
-      <v-col :sm="1">
       <a href="test" class="NavigationBar--Link-KennisPlein">KennisPlein</a>
-      </v-col>
-            <v-col :sm="1">
       <a href="test" class="NavigationBar--Link-Experts">Experts</a>
-      </v-col>
-            <v-col :sm="1">
       <a href="test" class="NavigationBar--Link-Leeractiviteiten">Leeractiviteiten</a>
-      </v-col>
-            <v-col :sm="2">
       <a href="test" class="NavigationBar--Link-Ontwikkeldossier">Mijn Ontwikkeldossier</a>
-      </v-col>
-      </v-row>
-    <slot />
+
+  </v-toolbar>
+    
   </div>
 </template>
 
@@ -40,27 +29,19 @@ opacity: 1;
 height: 130px;
 }
 
-a{
-padding-top: 100px;
-transform: translateY(-90%);
-}
-
-h1 {
-  padding-left: 60px;
-}
-
 .NavigationBar--logo {
-opacity: 1;
-height: 19px;
-padding-top: 25px;
-transform: translateY(-50%);
-padding-left: 240px;
+  padding-left: 20px;
+  padding-right: 20px;
 }
 
 .NavigationBar--Search {
-height: 19px;
-transform: translateY(50%);
-padding-left: 440px;
+border-radius: 20px;
+width: 295px;
+
+}
+
+a {
+padding: 0px 10px 0px 10px;
 }
 
 </style>
--- src/components/BannerItems/TopBannerBackgroundLight.vue
+++ src/components/BannerItems/TopBannerBackgroundLight.vue
@@ -12,7 +12,7 @@ background: var(--unnamed-color-6dc8bf) 0% 0% no-repeat padding-box;
 background: #6DC8BF 0% 0% no-repeat padding-box;
 border-radius: 0px 0px 160px 0px;
 opacity: 0.2;
-height: 728px;
+height: 600px;
 width: 90%;
 padding-top: 600px;
 

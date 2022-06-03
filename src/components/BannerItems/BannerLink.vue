a6377611448a437083bdc5f4a2e379180dee7c2a
GROUP\1035164 <kim.wauben@conclusion.nl>
2022-06-03 13:36:46 +0200

    created a package.json

--- src/components/BannerItems/BannerLink.vue
+++ src/components/BannerItems/BannerLink.vue
@@ -1,7 +1,7 @@
 <template>
 
 <div class="PageLinks pt-5">
-      <p class="PageLinks--Tekst">  <v-icon class="icon--medal">mdi-home</v-icon> <v-icon>mdi-chevron-right</v-icon> <span>Mijn Ontwikkeldossier</span>  <v-icon>mdi-chevron-right</v-icon> Inschrijvingen</p>
+      <p class="PageLinks--Tekst">  <v-icon class="icon--home">mdi-home</v-icon> <v-icon>mdi-chevron-right</v-icon> <span>Mijn Ontwikkeldossier</span>  <v-icon>mdi-chevron-right</v-icon> Inschrijvingen</p>
 </div>
 </template>
 
@@ -10,7 +10,7 @@
 .PageLinks--Tekst {
 color: #6EEB83;
   padding-bottom: 20px;
-  padding-left: 10px;
+
   position: relative;
 }
 
